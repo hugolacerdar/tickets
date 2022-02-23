9cf77d3fd6d4261177e9adcc1fbc0bcf0903205c
Hugo <hugolacerdar@gmail.com>
2022-02-23 15:41:37 -0300

    chore: add prepare messages implementation

--- lib/bookings_pipeline.ex
+++ lib/bookings_pipeline.ex
@@ -24,6 +24,33 @@ defmodule BookingsPipeline do
     end
 
     def handle_message(_processor, message, _context) do
+        %{data: %{event: event, user: user}} = message
+
+        # TODO: check for tickets availability
+        Tickets.create_ticket(user, event)
+        Tickets.send_email(user)
+        
         IO.inspect(message, label: "Message")
+    end
+
+    def prepare_messages(messages, _context) do
+        # Parse data and convert to a map.
+        messages = 
+            Enum.map(messages, fn message -> 
+                Broadway.Message.update_data(message, fn data -> 
+                    [event, user_id] = String.split(data, ",")
+                    %{event: event, user_id: user_id} 
+                end) 
+            end)
+
+        users = Tickets.users_by_ids(Enum.map(messages, & &1.data.user_id))
+
+        # Put users in messages.
+        Enum.map(messages, fn message -> 
+            Broadway.Message.update_data(message, fn data -> 
+                user = Enum.find(users, & &1.id == data.user_id)
+                Map.put(data, :user, user)
+            end)
+        end)
     end
 end
