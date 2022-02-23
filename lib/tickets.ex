a49216e857c479bce8f7809a38ea31b4338d3607
Hugo <hugolacerdar@gmail.com>
2022-02-23 15:07:59 -0300

    chore: add mocked business logic

--- lib/tickets.ex
+++ lib/tickets.ex
@@ -1,18 +1,25 @@
 defmodule Tickets do
-  @moduledoc """
-  Documentation for `Tickets`.
-  """
+  # Mocked business logic
+  def tickets_available?(_event) do
+    Process.sleep(Enum.random(100..200))
+    true
+  end
+  def create_ticket(_user, _event) do
+    Process.sleep(Enum.random(250..1000))
+  end
 
-  @doc """
-  Hello world.
+  def send_email(_user) do
+    Process.sleep(Enum.random(100..250))
+  end
 
-  ## Examples
+  @users [
+    %{id: "1", email: "only@example.com"},
+    %{id: "2", email: "a@example.com"},
+    %{id: "3", email: "mock@example.com"},
+  ]
 
-      iex> Tickets.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def user_by_ids(ids) when is_list(ids) do
+    Process.sleep(Enum.random(100..200))
+    Enum.filter(@users, &1.id in ids)
   end
 end
